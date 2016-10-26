0d8263b57c77a12b5985941cbf671f6dd322d475
Sebmansigh <sjs14b@acu.edu>
2016-10-25 20:40:59 -0500

    Update cucumber.bat

--- cucumber.bat
+++ cucumber.bat
@@ -1,2 +1,3 @@
 javac -cp "jars/*;." step_definitions/ClassMoverTest.java implementation/ClassMover.java
 java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features
+java -cp "jars/*;." cucumber.api.cli.Main -p progress --snippets camelcase -g step_definitions features
