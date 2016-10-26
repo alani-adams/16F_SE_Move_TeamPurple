7cb8120457afeffc5fd6c89049b32bece5a1137e
Sebmansigh <sjs14b@acu.edu>
2016-10-25 19:22:44 -0500

    Add files via upload

---
+++ implementation/BaseClass.java
@@ -0,0 +1,27 @@
+//
+//  THIS IS A SKELETON. ON EACH NEW ECLIPSE PROJECT:
+//    Edit the batch file to reflect the actual test class and base class files.
+//    Edit the comment below to match the batch file.
+//    Properly set Cucumber's run configuration.
+//    Remove this checklist comment.
+//    
+
+//
+//  Compile with:
+//      javac -cp "jars/*;." step_definitions/BaseTest.java implementation/BaseClass.java
+//      java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features
+//
+package implementation;
+
+
+/**
+ * 
+ * @author Sebastian Snyder
+ */
+public class BaseClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("YO!");
+	}
+}
