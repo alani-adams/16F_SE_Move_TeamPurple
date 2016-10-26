9aa4b3b60c9599e5c4326c5f73a626f2b98465a1
Sebmansigh <sjs14b@acu.edu>
2016-10-25 20:33:46 -0500

    Update cucumber.bat

--- cucumber.bat
+++ cucumber.bat
@@ -1,2 +1,2 @@
-javac -cp "jars/*;." step_definitions/BaseTest.java implementation/BaseClass.java
+javac -cp "jars/*;." step_definitions/ClassMoverTest.java implementation/ClassMover.java
 java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features
