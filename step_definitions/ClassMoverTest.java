60c0f5a82f4b48445980794e8020b678a69db6c2
alp13d <alp13d@acu.edu>
2016-11-01 22:38:45 -0500

    Update ClassMoverTest.java

--- step_definitions/ClassMoverTest.java
+++ step_definitions/ClassMoverTest.java
@@ -24,6 +24,12 @@ public class ClassMoverTest
 	{
 		assertTrue(true);
 	}
+	@Given("^Student with \"(.*?)\" is enrolled \"(.*?)\"$")
+    public void studentWithIsEnrolled(String banner, String course) throws Throwable 
+    	{
+        
+        //stuff here
+    	}
 }
 
 /**
