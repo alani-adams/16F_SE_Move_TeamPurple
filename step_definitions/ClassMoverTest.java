b582400f917f456cca2138f8fd4f1f8ac05753c2
Sebmansigh <sjs14b@acu.edu>
2016-11-07 22:22:37 -0600

    Add files via upload

--- step_definitions/ClassMoverTest.java
+++ step_definitions/ClassMoverTest.java
@@ -9,60 +9,53 @@ import cucumber.api.java.Before;
  */
 public class ClassMoverTest
 {
-	private ClassMover C;
+	private static ClassMover C;
+	
+	Student TestStu;
+	Professor TestProf;
+	String TestTerm;
+	Day TestDay;
+	short TestStartTime;
+	short TestEndTime;
 	
 	@Before
 	public void Before() throws Exception
 	{
-		CSV.DEBUG_MODE = true;
-		C = new ClassMover();
-	}
-
-
-	@Given("^A \"(.*?)\" teaches \"(.*?)\"$")
-	public void aTeaches(String professor, String course) throws Throwable 
-	{
-    	Professor p = C.getProfessorsMap().get(professor);
-    	Course c = C.getCoursesMap().get(course);
-    	Assert.assertTrue(c.getInstructor() == p);
-	}
-
-	@Given("^Student with \"(.*?)\" is enrolled \"(.*?)\"$")
-	public void studentWithIsEnrolled(String banner, String course) throws Throwable 
-	{
-	    Student s = C.getStudentsMap().get(banner);
-	    Course c = C.getCoursesMap().get(course);
-	    Assert.assertTrue(s.getCourseSet().contains(c));
+		if(C == null)
+		{
+			C = new ClassMover();
+		}
 	}
 	
 	@Given("^Student \"(.*?)\" on \"(.*?)\" between \"(.*?)\" and \"(.*?)\" in term \"(.*?)\"$")
-	public void studentOnBetweenAndInTerm(String banner, String day, String start, String end, String term) throws Throwable 
-	{
-    	// Write code here that turns the phrase above into concrete actions
-    	throw new PendingException();
+	public void student_on_between_and_in_term(String Banner, String DayChar, String Ts, String Te, String TermCode) throws Throwable {
+	    TestStu = C.getStudentsMap().get(Banner);
+	    TestDay = Day.getFromChar(DayChar.charAt(0));
+	    TestTerm = TermCode;
+	    TestStartTime = Short.parseShort(Ts);
+	    TestEndTime = Short.parseShort(Te);
+	    Assert.assertNotNull(TestStu);
+	}
+	
+	@Then("^the student \"(.*?)\" available$")
+	public void the_student_available(String is) throws Throwable {
+	    Assert.assertTrue(is.equals("is not") ^ TestStu.SlotFree(TestTerm, TestDay, TestStartTime, TestEndTime));
 	}
 
-	@Then("^the student \"(.*?)\" available$")
-	public void theStudentAvailable(String availability) throws Throwable 
-	{
-    	// Write code here that turns the phrase above into concrete actions
-    	throw new PendingException();
-	}
 	@Given("^Professor \"(.*?)\" on \"(.*?)\" between \"(.*?)\" and \"(.*?)\" in term \"(.*?)\"$")
-	public void professorOnBetweenAndInTerm(String prof, String day, String start, String end, String term) throws Throwable 
-	{
-    	// Write code here that turns the phrase above into concrete actions
-    	throw new PendingException();
+	public void professor_on_between_and_in_term(String Pname, String DayChar, String Ts, String Te, String TermCode) throws Throwable {
+		TestProf = C.getProfessorsMap().get(Pname);
+	    TestDay = Day.getFromChar(DayChar.charAt(0));
+	    TestTerm = TermCode;
+	    TestStartTime = Short.parseShort(Ts);
+	    TestEndTime = Short.parseShort(Te);
+	    Assert.assertNotNull(TestProf);
 	}
 
 	@Then("^the Professor \"(.*?)\" available$")
-	public void theProfessorAvailable(String availability) throws Throwable 
-	{
-    	// Write code here that turns the phrase above into concrete actions
-    	throw new PendingException();
+	public void the_Professor_available(String is) throws Throwable {
+	    Assert.assertTrue(is.equals("is not") ^ TestProf.SlotFree(TestTerm, TestDay, TestStartTime, TestEndTime));
 	}
-
-
 }
 
 /**
