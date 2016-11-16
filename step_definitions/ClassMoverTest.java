ac6b542a715b05d1e351e5b33b273cfb181cfa36
Sebmansigh <sjs14b@acu.edu>
2016-11-15 21:17:33 -0600

    Add files via upload

--- step_definitions/ClassMoverTest.java
+++ step_definitions/ClassMoverTest.java
@@ -3,6 +3,11 @@ package step_definitions;
 import implementation.*;
 import junit.framework.Assert;
 import cucumber.api.java.en.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import cucumber.api.java.Before;
 /**
  * @author Sebastian Snyder
@@ -15,6 +20,7 @@ public class ClassMoverTest
 	Professor TestProf;
 	String TestTerm;
 	Day TestDay;
+	Course TestCourse;
 	short TestStartTime;
 	short TestEndTime;
 	
@@ -55,6 +61,33 @@ public class ClassMoverTest
 	@Then("^the Professor \"(.*?)\" available$")
 	public void the_Professor_available(String is) throws Throwable {
 	    Assert.assertTrue(is.equals("is not") ^ TestProf.SlotFree(TestTerm, TestDay, TestStartTime, TestEndTime));
+	}
+	
+	
+	@Given("^course \"(.*?)\" and time slot \"(.*?)\" to \"(.*?)\" on \"(.*?)\"$")
+	public void course_and_and_and(String arg1, short arg2, short arg3, char arg4) throws Throwable {
+		String[] Data = arg1.split(" ");
+		//System.out.println(Arrays.toString(Data));
+		HashMap<String,Course> Cmap = C.getCoursesMap(Data[0]);
+		//System.out.println(Cmap);
+	    TestCourse = Cmap.get(Data[1]);
+	    TestDay = Day.getFromChar(arg4);
+	    TestStartTime = arg2;
+	    TestEndTime = arg3;
+	    Assert.assertNotNull(TestCourse);
+	}
+
+	@Then("^\"(.*?)\" amount of students have a conflict")
+	public void amount_of_students_have_a_conflict_and_the_professor_have_a_conflict(int arg1) throws Throwable
+	{
+	    Assert.assertEquals(arg1, C.GetStudentConflicts(TestCourse, TestDay, TestStartTime, TestEndTime).size());
+	}
+	
+	@Then("^the professor \"(.*?)\" have a conflict$")
+	public void the_professor_have_a_conflict(String arg1) throws Throwable
+	{
+		//System.out.println(C.CanMoveProfessor(TestCourse, TestDay, TestStartTime, TestEndTime));
+	    Assert.assertTrue(arg1.equals("does") ^ C.CanMoveProfessor(TestCourse, TestDay, TestStartTime, TestEndTime));
 	}
 }
 
